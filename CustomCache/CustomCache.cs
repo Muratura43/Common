3e5e172676b79a6d755f369801120f0e72862e9a
Bogdan Radu <radu.bogdan17@gmail.com>
2024-07-27 20:38:16 +0300

    added CustomCache

---
+++ CustomCache/CustomCache.cs
@@ -0,0 +1,71 @@
+﻿namespace CustomCache
+{
+    public class CustomCache<TKey, TValue> where TKey : IEquatable<TKey>
+    {
+        private readonly object _lock = new object();
+        private readonly Dictionary<TKey, TimedObject<TValue>> _cache;
+
+        public CustomCache()
+        {
+            _cache = new Dictionary<TKey, TimedObject<TValue>>();
+        }
+
+        public bool AddItem(TKey key, TValue value, TimeSpan time)
+        {
+            try
+            {
+                lock (_lock)
+                {
+                    var n = new TimedObject<TValue>(value, time);
+                    if (!_cache.TryAdd(key, n))
+                    {
+                        _cache[key] = n;
+                    }
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public TValue? GetItem(TKey key)
+        {
+            var val = _cache[key];
+
+            if (val != null)
+            {
+                if (val.IsValid)
+                {
+                    return val.Value;
+                }
+                else
+                {
+                    lock (_lock)
+                    {
+                        _cache.Remove(key);
+                    }
+                }
+            }
+
+            return default;
+        }
+
+        private class TimedObject<TValueObject>
+        {
+            public TValue? Value { get; set; }
+
+            private DateTimeOffset _createdTime;
+
+            public TimedObject(TValue value, TimeSpan time)
+            {
+                Value = value;
+                _createdTime = DateTimeOffset.Now.Add(time);
+            }
+
+            public bool IsValid => _createdTime > DateTimeOffset.Now;
+        }
+    }
+}
---
+++ Tests/CustomCacheTests.cs
@@ -0,0 +1,15 @@
+﻿using CustomCache;
+
+namespace Tests
+{
+    public class CustomCacheTests
+    {
+        [Test]
+        public void TestCustomCache()
+        {
+            var cache = new CustomCache<Guid, object>();
+
+            //TODO:
+        }
+    }
+}
